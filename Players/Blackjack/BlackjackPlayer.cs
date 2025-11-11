d92d667fea783de59b363a27f3bffd18d737c67b
Patricio Reyes Álvarez <reyesalvarez.patricio@gmail.com>
2025-11-10 23:36:26 -0600

    Creation of blackjack player functionalities

--- Players/Blackjack/BlackjackPlayer.cs
+++ Players/Blackjack/BlackjackPlayer.cs
@@ -1,3 +1,4 @@
+using card_games_sim.Cards;
 using card_games_sim.Games;
 
 namespace card_games_sim.Players;
@@ -9,9 +10,57 @@ public class BlackjackPlayer : Player
 
   public int Victories { get; set; }
   public int Value { get; private set; }
+  protected bool ShouldPlay { get; set; } = true;
 
   public override void Play(Game game)
   {
-    throw new NotImplementedException();
+    while (ShouldPlay)
+    {
+      if (Value > 21)
+      {
+        Console.WriteLine($"{Name} busted");
+        break;
+      }
+
+      int rnd = Random.Shared.Next(0, 2);
+      if (rnd == 0)
+        Hit(((BlackjackGame)game).Deck);
+      else
+      {
+        Stand();
+      }
+    }
+    ShouldPlay = true;
+  }
+
+  protected void Play(int pointOfCut, Game game)
+  {
+    while (ShouldPlay)
+    {
+      if (Value > 21)
+      {
+        Console.WriteLine($"{Name} busted");
+        break;
+      }
+      if (Value >= pointOfCut)
+      {
+        Stand();
+        break;
+      }
+      Hit(((BlackjackGame)game).Deck);
+    }
+    ShouldPlay = true;
+  }
+
+  protected void Hit(Deck deck)
+  {
+    Console.WriteLine($"{Name} hits");
+    AddCardToHand(deck.Cards.Pop());
+  }
+
+  protected void Stand()
+  {
+    Console.WriteLine($"{Name} stands");
+    ShouldPlay = false;
   }
 }
