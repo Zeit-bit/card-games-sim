c82222e66d79be46562ab1627de96794f9327915
milliealgr <agaal024@gmail.com>
2025-11-11 18:10:24 -0600

    Fix some logic behind the blackjack game functionality

--- Players/Uno/UnoPlayer.cs
+++ Players/Uno/UnoPlayer.cs
@@ -1,8 +1,11 @@
+using card_games_sim.Cards;
 using card_games_sim.Games;
+using card_games_sim.Interfaces;
+using card_games_sim.Utils;
 
 namespace card_games_sim.Players;
 
-public class UnoPlayer : Player
+public class UnoPlayer : Player, IShuffler, IDealer
 {
   public UnoPlayer(string name)
     : base(name) { }
@@ -10,5 +13,15 @@ public class UnoPlayer : Player
   public override void Play(Game game)
   {
     throw new NotImplementedException();
+  }
+
+  public void DealCards(int qtyToDeal, Deck deck, Player[] players)
+  {
+    DeckManager.DealCards(qtyToDeal, deck, players);
+  }
+
+  public void ShuffleCards(Deck deck)
+  {
+    DeckManager.ShuffleCards(deck);
   }
 }
